47b96feb5b020e20ef4ac3fbc1f5d1b55c3d181b
cctRoisinKeenan <sba24446@student.cct.ie>
2024-12-28 11:34:32 +0000

    stramlet required

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import json
 import pandas as pd
-import plotly.express as px  # (version 4.7.0)
+import plotly.express as px  
 import plotly.graph_objects as go
 
 import dash
